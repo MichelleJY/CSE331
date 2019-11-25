3870890cf88df0d4de1d238775987210b430f448
Xiaowei Chen <xiaowei233.chen@gmail.com>
2019-11-25 10:45:28 -0500

    Add new UI to photo uploads

--- photo-sharing-app/src/app/upload-photo/photo-upload.component.ts
+++ photo-sharing-app/src/app/upload-photo/photo-upload.component.ts
@@ -1,67 +1,82 @@
-import { Component, OnInit  } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { AllService } from '../all.service';
+import { Component, OnInit } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { AllService } from "../all.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'photo-upload',
-  templateUrl: './photo-upload.component.html',
-  styleUrls: ['./photo-upload.component.scss']
+  selector: "photo-upload",
+  templateUrl: "./photo-upload.component.html",
+  styleUrls: ["./photo-upload.component.scss"]
 })
 export class PhotoUploadComponent {
-    images: any;
-//   title = 'PROJECT-NAME';
-    $path_to_backend = 'https://das-lab.org/cse331fa2019/PhotosBackend/';
-    imageArr: any[] = [];
-    src: any[] = [];
-    des: any;
-    description: any = {};
-//   photoId: any;
-  eventTag:any;
-
- constructor(private appserive: AllService) {
-  }
+  images: any;
+  //   title = 'PROJECT-NAME';
+  $path_to_backend = "https://das-lab.org/cse331fa2019/PhotosBackend/";
+  imageArr: any[] = [];
+  src: any[] = [];
+  des: any;
+  description: any = {};
+  //   photoId: any;
+  eventTag: any;
+  popup: boolean = false;
+  confirm:boolean = false;
+  constructor(private router: Router, private appserive: AllService) {}
 
   upload() {
-    var albumId = '_' + Math.random().toString(36).substr(2, 9); //? rand + photo id ?
+    var albumId =
+      "_" +
+      Math.random()
+        .toString(36)
+        .substr(2, 9); //? rand + photo id ?
     var cover = true;
-    for (let src of this.src)
-    {
+    for (let src of this.src) {
       const formData = new FormData();
-      formData.append('fupload', src);
-      formData.append('grp_id', '23333');
+      formData.append("fupload", src);
+      formData.append("grp_id", "23333");
       this.description.albumId = albumId;
       this.description.cover = cover;
       cover = false;
       this.description.create_date = new Date();
       this.description.description = this.des;
       this.description.event = this.eventTag;
-      formData.append('description', JSON.stringify(this.description));
+      formData.append("description", JSON.stringify(this.description));
       this.appserive.postOne(formData).subscribe(
-        (res) => console.log(res),
-        (err) => console.log(err)
+        res => {
+          this.confirm = true;
+          setTimeout(() => {
+            this.router.navigate(["/timeline"]);
+          }, 2000);
+        },
+        err => console.log(err)
       );
     }
   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onChanged($event) {
+    var oFReader = new FileReader();
+    oFReader.readAsDataURL($event.target.files[0]);
+    this.src.push($event.target.files[0]);
+    oFReader.onload = oFREvent => {
+      this.imageArr.push(oFReader.result);
+    };
   }
 
-  onChanged($event)
-  {
-      var oFReader = new FileReader();
-      oFReader.readAsDataURL($event.target.files[0]);
-      this.src.push($event.target.files[0]);
-      oFReader.onload = (oFREvent) => {
-          this.imageArr.push(oFReader.result);
-      };
-  }
-
-  removeImg(event)
-  {
+  removeImg(event) {
     var id = this.imageArr.indexOf(event);
     this.imageArr.splice(id, 1);
-    this.src.splice(id, 1)
+    this.src.splice(id, 1);
+  }
+
+  cancel() {
+    this.popup = !this.popup;
+    console.log(this.popup);
+  }
+
+  exit() {
+    this.router.navigate(["/timeline"]);
   }
 }
